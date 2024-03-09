0fe2409448eed606bcb0b814d24e5becd3968849
Anthony Hou <tonyhou@live.com>
2024-03-09 13:24:32 -0800

    Updated to handle events

--- index.js
+++ index.js
@@ -27,5 +27,19 @@ for (const folder of commandFolders) {
 	}
 }
 
+// Handle events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 // Log in to Discord with your client's token
 client.login(token);
